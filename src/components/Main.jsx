9bc95b92127e50ddb00552fe98872c069e69163d
Hanan Munir <66138578+hananmunir@users.noreply.github.com>
2023-05-12 22:26:33 +0500

    Fixed Bugs

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,15 +8,18 @@ import { SpotLight } from "@react-three/drei";
 import { useControls } from "leva";
 
 //initial state of spot light controls
-const intensityValue = 200;
-const distanceValue = 224;
-const angleValue = 0.09;
-const penumbraValue = 0.05;
-const decayValue = 1;
+const map = "disturb.jpg";
+const intensity = 200;
+const distance = 150;
+const angle = 0.11;
+const penumbra = 0.53;
+const decay = 1.2;
 const focus = 1;
 const x = 97,
   y = 60,
   z = 34;
+
+const color = "#157b8d";
 
 const LucyModel = () => {
   const gltf = useLoader(GLTFLoader, "/models/Lucy100k.gltf");
@@ -34,10 +37,14 @@ const LucyModel = () => {
     camera.position.z = z;
   });
 
+  useEffect(() => {
+    if (model) model.rotateY(THREE.MathUtils.degToRad(250));
+  }, [model]);
+
   // decrease the size of the model
   model.scale.set(0.12, 0.12, 0.12);
   // model.position.set(5, 75, 0);
-  model.rotateY(THREE.MathUtils.degToRad(230));
+
   // use the useRef hook to access the model mesh
   const meshRef = useRef();
   // set the position of the model based on the values of the controls
@@ -61,7 +68,7 @@ export default function Main() {
       const texture = loader.load(filename, () => {
         texture.minFilter = THREE.LinearFilter;
         texture.magFilter = THREE.LinearFilter;
-        texture.encoding = THREE.sRGBEncoding;
+        texture.colorSpace = THREE.sRGBEncoding;
       });
       newTextures[filename] = texture;
     }
@@ -69,20 +76,20 @@ export default function Main() {
   }, []);
 
   //initial state of spot light controls
-  const spotlightControls = useControls({
-    color: "#11a3c4",
-    intensity: { value: intensityValue, min: 10, max: 200, step: 10 },
-    distance: { value: distanceValue, min: 150, max: 500, step: 10 },
-    //to add a texture to the light, we need to use the texture object
-    map: {
-      value: "disturb.jpg",
-      options: ["none", "disturb.jpg", "colors.png", "uv_grid_opengl.jpg"],
-    },
-    angle: { value: angleValue, min: 0.05, max: Math.PI / 2, step: 0.01 },
-    penumbra: { value: penumbraValue, min: 0, max: 1, step: 0.01 },
-    decay: { value: decayValue, min: 1, max: 2, step: 0.01 },
-    focus: { value: focus, min: -3, max: 3, step: 0.05 },
-  });
+  // const spotlightControls = useControls({
+  //   color: "#157b8d",
+  //   intensity: { value: intensityValue, min: 10, max: 200, step: 10 },
+  //   distance: { value: distanceValue, min: 150, max: 500, step: 10 },
+  //   //to add a texture to the light, we need to use the texture object
+  //   map: {
+  //     value: "disturb.jpg",
+  //     options: ["none", "disturb.jpg", "colors.png", "uv_grid_opengl.jpg"],
+  //   },
+  //   angle: { value: angleValue, min: 0.05, max: Math.PI / 2, step: 0.01 },
+  //   penumbra: { value: penumbraValue, min: 0, max: 1, step: 0.01 },
+  //   decay: { value: decayValue, min: 1, max: 2, step: 0.01 },
+  //   focus: { value: focus, min: -3, max: 3, step: 0.05 },
+  // });
 
   //to rotate spot light continuously
   const RotateLight = () => {
@@ -94,19 +101,19 @@ export default function Main() {
       <spotLight
         ref={spotLightRef}
         scale={[10, 10, 10]}
-        color={spotlightControls.color}
-        intensity={spotlightControls.intensity}
-        distance={spotlightControls.distance}
-        map={textures && textures[spotlightControls.map]}
-        angle={spotlightControls.angle}
-        penumbra={spotlightControls.penumbra}
-        decay={spotlightControls.decay}
+        color={color}
+        intensity={intensity}
+        distance={distance}
+        map={textures && textures[map]}
+        angle={angle}
+        penumbra={penumbra}
+        decay={decay}
         castShadow
         shadow-mapSize-width={1024}
         shadow-mapSize-height={1024}
         shadow-camera-near={10}
         shadow-camera-far={200}
-        shadow-focus={spotlightControls.focus}
+        shadow-focus={focus}
       />
     );
   };
@@ -133,7 +140,7 @@ export default function Main() {
             far: 1000,
           }}
         >
-          <pointLight position={[600, 35, 300]} castShadow />
+          <pointLight position={[600, 35, 300]} castShadow intensity={0.2} />
           {/* <OrbitControls
           minDistance={20}
           maxDistance={2000}
@@ -141,7 +148,7 @@ export default function Main() {
         /> */}
           <group scale={[0.09, 0.09, 0.09]} position={[0, -2, 0]}>
             <mesh rotation-x={-Math.PI / 2} position={[0, -1, -10]}>
-              <planeBufferGeometry args={[2000, 2000]} />
+              <planeGeometry args={[2000, 2000]} />
               <meshLambertMaterial color={0x808080} />
             </mesh>
 
@@ -172,8 +179,8 @@ export default function Main() {
       >
         <span
           style={{
-            fontSize: "80px",
-            lineHeight: "100px",
+            fontSize: "70px",
+            lineHeight: "80px",
             marginBottom: "1rem",
           }}
         >
